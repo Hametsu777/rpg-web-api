910fdaf2729bc0cb0bf8c4c1f343326c778e4f92
Justin Moore <hametsu777@gmail.com>
2023-08-28 15:48:31 -0400

    Add Models and Controller

---
+++ RPGWebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RPGWebAPI.Models;
+
+namespace RPGWebAPI.Controllers
+{
+    // ApiController enables attribute routing and automatic HTTP 400 responses.
+    // Route helps find a specific controller when we want to make a service call.
+    // [Controller] in route is the name of the controller(Character), minus the controller part.
+    // IActionResult allows us to send back HHTP status codes with data that client requested.
+    // ActionResult<Character> sends character schema as well.
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        public static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character{Id = 1, Name = "Sam"}
+        };
+
+        [HttpGet("/GetAll")]
+        public ActionResult<List<Character>> GetAllCharacters()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("/GetSingle")]
+        public ActionResult<Character> GetSingleCharacter(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
---
+++ RPGWebAPI/Models/RpgClass.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace RPGWebAPI.Models
+{
+    // [JsonConverter(typeof(JsonStringEnumConverter))] converts enum values to string.
+    // This is used because the classes were showing up as integers(1, 2, 3) in schema.
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum RpgClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
